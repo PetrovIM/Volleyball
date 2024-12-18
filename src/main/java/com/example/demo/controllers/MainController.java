0541f64fcc1686765ee4903228a06abc0f1c723b
Ilya Petrv <petrov_im@rambler.ru>
2024-12-18 12:03:26 +0300

    Переход на thymeleaf

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -7,6 +7,7 @@ import com.example.demo.repo.ReviewRepository;
 import com.example.demo.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,52 +25,52 @@ public class   MainController {
     private UserRepository userRepository;
 
     @GetMapping
-    public String home(Map<String,Object> model){
-        model.put("name", "Ilya");
+    public String home(Model model){
+        model.addAttribute("name", "Ilya");
         return "home";
     }
 
     @GetMapping("/about")
-    public String about(Map<String,Object>model){
-        model.put("title", "About us");
+    public String about(Model model){
+        model.addAttribute("title", "About us");
         return "about";
     }
 
     @GetMapping("/reviews")
-    public String reviews(Map<String,Object>model){
+    public String reviews(Model model){
         Iterable<Review> reviews = reviewRepository.findAll();
-        model.put("title", "Feedback");
-        model.put("reviews",reviews);
+        model.addAttribute("title", "Feedback");
+        model.addAttribute("reviews",reviews);
         return "reviews";
     }
 
     @PostMapping("/reviews/add")
-    public String reviewsAdd(@RequestParam String title, @RequestParam String text, Map<String,Object> model){
+    public String reviewsAdd(@RequestParam String title, @RequestParam String text, Model model){
         Review review = new Review(title, text);
         reviewRepository.save(review);
         return "redirect:/reviews";
     }
 
     @GetMapping("/reviews/{id}")
-    public String reviewInfo(@PathVariable(value = "id") long review_id, Map<String,Object> model){
+    public String reviewInfo(@PathVariable(value = "id") long review_id, Model model){
         Optional<Review> review = reviewRepository.findById(review_id);
         ArrayList<Review> result = new ArrayList<>();
         review.ifPresent(result::add);
-        model.put("reviews",result);
+        model.addAttribute("reviews",result);
         return "review-info";
     }
 
     @GetMapping("/reviews/{id}/update")
-    public String reviewUpdate(@PathVariable(value = "id") long review_id, Map<String,Object> model){
+    public String reviewUpdate(@PathVariable(value = "id") long review_id, Model model){
         Optional<Review> review = reviewRepository.findById(review_id);
         ArrayList<Review> result = new ArrayList<>();
         review.ifPresent(result::add);
-        model.put("reviews",result);
+        model.addAttribute("reviews",result);
         return "review-update";
     }
 
     @PostMapping("/reviews/{id}/update")
-    public String reviewsUpdateForm(@PathVariable(value = "id") long review_id, @RequestParam String title, @RequestParam String text, Map<String,Object> model){
+    public String reviewsUpdateForm(@PathVariable(value = "id") long review_id, @RequestParam String title, @RequestParam String text, Model model){
         Review review = reviewRepository.findById(review_id).orElseThrow();
         review.setTitle(title);
         review.setText(text);
@@ -83,7 +84,7 @@ public class   MainController {
     }
 
     @PostMapping("/reg")
-    public String addUsers(User user, Map<String, Object> model){
+    public String addUsers(User user, Model model){
         user.setEnabled(true);
         user.setRole(Collections.singleton(Role.USER));
         userRepository.save(user);
